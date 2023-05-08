3a200ef28e8105b40f49b5cbf10603436b531cf3
ravbhart <ravi.prakash.bharti@oracle.com>
2023-05-08 16:15:56 +0530

    Added custom scripts

---
+++ scripts/issue_label_manager.js
@@ -0,0 +1,21 @@
+
+module.exports = ({github, context}) => {
+    console.log(context.payload.client_payload.value)
+    const query = `query($owner:String!, $name:String!, $label:String!) {
+              repository(owner:$owner, name:$name){
+                issues(first:100, labels: [$label]) {
+                  nodes {
+                    id
+                  }
+                }
+              }
+            }`;
+    const variables = {
+        owner: context.repo.owner,
+        name: context.repo.repo,
+        label: 'Pending Engineering'
+    }
+    const result = github.graphql(query, variables);
+    console.log(result);
+    return context.payload.client_payload.value;
+}
