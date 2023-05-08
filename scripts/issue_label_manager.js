d83c07713190e7c72f247e868aa885074da8126f
ravbhart <ravi.prakash.bharti@oracle.com>
2023-05-08 17:57:38 +0530

    added

--- scripts/issue_label_manager.js
+++ scripts/issue_label_manager.js
@@ -1,5 +1,5 @@
 
-module.exports = ({github, context}) => {
+module.exports = async ({github, context}) => {
 //     console.log(context.payload.client_payload.value)
     const query = `query($owner:String!, $name:String!, $label:String!) {
               repository(owner:$owner, name:$name){
@@ -16,13 +16,13 @@ module.exports = ({github, context}) => {
         label: 'Pending Engineering'
     }
     const result = github.graphql(query, variables);
-    console.log(context.issue.number);
-    github.rest.issues.createComment({
-        issue_number: context.issue.number,
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        body: '👋 comment from action'
-    });
-    console.log(result);
+    // console.log(context.issue.number);
+    // github.rest.issues.createComment({
+    //     issue_number: context.issue.number,
+    //     owner: context.repo.owner,
+    //     repo: context.repo.repo,
+    //     body: '👋 comment from action'
+    // });
+    // console.log(result);
     return result;
 }
