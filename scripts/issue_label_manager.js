044e74146d07f7da738972c4389833971f0fc186
ravbhart <ravi.prakash.bharti@oracle.com>
2023-05-08 17:46:56 +0530

    added fake comment

--- scripts/issue_label_manager.js
+++ scripts/issue_label_manager.js
@@ -16,6 +16,13 @@ module.exports = ({github, context}) => {
         label: 'Pending Engineering'
     }
     const result = github.graphql(query, variables);
+    console.log(context.issue.number);
+    github.rest.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: '👋 comment from action'
+    });
     console.log(result);
     return result;
 }
